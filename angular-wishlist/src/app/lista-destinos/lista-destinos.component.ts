20204372a453e96f6c5545cf3016c23bd3571a78
StivenManco26 <manco.5@hotmail.com>
2020-07-19 16:30:09 -0500

    se agrega model destino-viaje. se agrega hostbinding

--- angular-wishlist/src/app/lista-destinos/lista-destinos.component.ts
+++ angular-wishlist/src/app/lista-destinos/lista-destinos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DestinoViaje } from './../models/destino-viaje.model';
 
 @Component({
   selector: 'app-lista-destinos',
@@ -6,12 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./lista-destinos.component.css']
 })
 export class ListaDestinosComponent implements OnInit {
-  destinos: string[];
+  destinos: DestinoVaije[];
   constructor() { 
-  	this.destinos = ['Barranquilla', 'Barcelona', 'Lima', 'Medellín']
+  	this.destinos = [];
   }
 
   ngOnInit(): void {
+  }
+
+  guardar(nombre:string, url:string):boolean{
+  	this.destinos.push(new DestinoViaje(nombre,url));
+  	console.log(this.destinos);
+  	return false;
   }
 
 }
