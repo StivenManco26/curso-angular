8fc9c84745ce8c26c23740ce44d3fbe9c4744fbf
StivenManco26 <manco.5@hotmail.com>
2020-07-26 15:22:17 -0500

    Se agregan validadores por defecto y validadores personalizados

--- angular-wishlist/src/app/destino-viaje/destino-viaje.component.ts
+++ angular-wishlist/src/app/destino-viaje/destino-viaje.component.ts
@@ -10,16 +10,16 @@ export class DestinoViajeComponent implements OnInit {
   @Input() destino: DestinoViaje;
   @Input('idx') posicion: number;
   @HostBinding('attr.class') cssClass = 'col-md-4'
-  @Output() clicked: EventEmitter<DestinoViaje>;
+  @Output() onClicked: EventEmitter<DestinoViaje>;
   constructor() {
-    this.clicked = new EventEmitter();
+    this.onClicked = new EventEmitter();
   }
 
   ngOnInit(): void {
   }
 
   ir(){
-    this.clicked.emit(this.destino);
+    this.onClicked.emit(this.destino);
     return false;
   }
 }
--- angular-wishlist/src/app/models/destino-viaje.model.ts
+++ angular-wishlist/src/app/models/destino-viaje.model.ts
@@ -1,6 +1,10 @@
+import {v4 as uuid} from 'uuid';
+
 export class DestinoViaje{
 	private selected: boolean;
 	servicios: string[];
+	id = uuid();
+	
 	constructor(public n:string, public u:string){
 		this.servicios = ['Piscina', 'Desayuno'];
 	}
