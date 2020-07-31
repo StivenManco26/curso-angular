1d3485e74d74eaf5a00835ed431b9fbad20e85f7
StivenManco26 <manco.5@hotmail.com>
2020-07-30 22:29:57 -0500

    Manejo de rutas avanzado y rutas anidadas

--- angular-wishlist/src/app/app.module.ts
+++ angular-wishlist/src/app/app.module.ts
@@ -8,22 +8,49 @@ import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { environment } from '../environments/environment'; // Angular CLI environemnt
 
 import { AppComponent } from './app.component';
-import { DestinoViajeComponent } from './destino-viaje/destino-viaje.component';
-import { ListaDestinosComponent } from './lista-destinos/lista-destinos.component';
-import { DestinoDetalleComponent } from './destino-detalle/destino-detalle.component';
+import { DestinoViajeComponent } from './components/destino-viaje/destino-viaje.component';
+import { ListaDestinosComponent } from './components/lista-destinos/lista-destinos.component';
+import { DestinoDetalleComponent } from './components/destino-detalle/destino-detalle.component';
 import { DestinosApiClient } from './models/destinos-api-client.model';
-import { FormDestinoViajeComponent } from './form-destino-viaje/form-destino-viaje.component';
+import { FormDestinoViajeComponent } from './components/form-destino-viaje/form-destino-viaje.component';
 import {
   DestinosViajesState,
   intializeDestinosViajesState,
   reducerDestinosViajes,
   DestinosViajesEffects
 } from './models/destinos-viajes-state.model';
+import { LoginComponent } from './components/login/login/login.component';
+import { ProtectedComponent } from './components/protected/protected/protected.component';
+import { UsuarioLogueadoGuard } from './guards/usuario-logueado/usuario-logueado.guard';
+import { AuthService } from './services/auth.service';
+import { VuelosComponentComponent } from './components/vuelos/vuelos-component/vuelos-component.component';
+import { VuelosMainComponentComponent } from './components/vuelos/vuelos-main-component/vuelos-main-component.component';
+import { VuelosMasInfoComponentComponent } from './components/vuelos/vuelos-mas-info-component/vuelos-mas-info-component.component';
+import { VuelosDetalleComponent } from './components/vuelos/vuelos-detalle-component/vuelos-detalle-component.component';
+
+export const childrenRoutesVuelos: Routes = [
+  { path: '', redirectTo: 'main', pathMatch: 'full' },
+  { path: 'main', component: VuelosMainComponentComponent },
+  { path: 'mas-info', component: VuelosMasInfoComponentComponent },
+  { path: ':id', component: VuelosDetalleComponent },
+];
 
 const routes: Routes = [
   {path: '', redirectTo: 'home', pathMatch: 'full'},
   {path: 'home', component: ListaDestinosComponent},
-  {path: 'destino/:id', component: DestinoDetalleComponent}
+  {path: 'destino/:id', component: DestinoDetalleComponent},
+  { path: 'login', component: LoginComponent },
+    {
+      path: 'protected',
+      component: ProtectedComponent,
+      canActivate: [ UsuarioLogueadoGuard ]
+    },
+    {
+      path: 'vuelos',
+      component: VuelosComponentComponent,
+      canActivate: [ UsuarioLogueadoGuard ],
+      children: childrenRoutesVuelos
+    }
 ];
 
 //redux init
@@ -46,7 +73,13 @@ let reducersInitialState = {
     DestinoViajeComponent,
     ListaDestinosComponent,
     DestinoDetalleComponent,
-    FormDestinoViajeComponent
+    FormDestinoViajeComponent,
+    LoginComponent,
+    ProtectedComponent,
+    VuelosComponentComponent,
+    VuelosMainComponentComponent,
+    VuelosMasInfoComponentComponent,
+    VuelosDetalleComponent
   ],
   imports: [
     BrowserModule,
@@ -64,7 +97,9 @@ let reducersInitialState = {
     StoreDevtoolsModule.instrument(),
   ],
   providers: [
-    DestinosApiClient
+    DestinosApiClient,
+    AuthService,
+    UsuarioLogueadoGuard
   ],
   bootstrap: [AppComponent]
 })
