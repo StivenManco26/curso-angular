f561891e53b25c47752ff99f75ab6bb2a7ef355e
StivenManco26 <manco.5@hotmail.com>
2020-08-08 13:07:43 -0500

    Pruebas unitarias con Jasmine

--- express-server/app.js
+++ express-server/app.js
@@ -11,6 +11,9 @@ var misDestinos = [];
 app.get("/my", (req, res, next) => res.json(misDestinos));
 app.post("/my", (req, res, next) => {
   console.log(req.body);
-  misDestinos = req.body;
+  misDestinos.push(req.body.nuevo);
   res.json(misDestinos);
 });
+app.get("/api/translation", (req, res, next) => res.json([
+  {lang: req.query.lang, key: 'HOLA', value: 'HOLA ' + req.query.lang}
+]));
